5f09f7b9206eb5482d24172c9aaa8bcebb8daa7f
pasqu <pasquale.bucciero97@gmailcom>
2021-05-17 17:38:57 +0200

    init footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,43 @@
+<footer>
+    <div class="containerFooter">
+        <div class="linkFooter">
+            <ul class="dcComics">
+                <li><h2>DC COMICS</h2></li>
+                <li> <a href="#">Characters</a> </li>
+                <li> <a href="#">Comics</a> </li>
+                <li> <a href="#">Movie</a> </li>
+                <li> <a href="#">TV</a> </li>
+                <li> <a href="#">Games</a> </li>
+                <li> <a href="#">Videos</a> </li>
+                <li> <a href="#">News</a> </li>
+            </ul>
+            <ul class="dc">
+                <li><h2>DC</h2></li>
+                <li> <a href="#">Terms of use</a> </li>
+                <li> <a href="#">Privacy Policy(new)</a> </li>
+                <li> <a href="#">Ad choices</a> </li>
+                <li> <a href="#">Advertsing</a> </li>
+                <li> <a href="#">Jobs</a> </li>
+                <li> <a href="#">Subscription</a> </li>
+                <li> <a href="#">Talent Workshops</a> </li>
+                <li> <a href="#">CPSC Certificates</a> </li>
+                <li> <a href="#">Ratings</a> </li>
+                <li> <a href="#">Shop Help</a> </li>
+                <li> <a href="#">Contact Us</a> </li>
+            </ul>
+            <ul class="sites">
+                <li><h2>SITES</h2></li>
+                <li> <a href="#">DC</a> </li>
+                <li> <a href="#">MAD Magazine</a> </li>
+                <li> <a href="#">DC Kids</a> </li>
+                <li> <a href="#">DC Universe</a> </li>
+                <li> <a href="#">DC Power Visa</a> </li>
+            </ul>
+            <ul class="shop">
+                <li><h2>Shop</h2></li>
+                <li> <a href="#">Shop DC</a> </li>
+                <li> <a href="#">Shop DC Collections</a> </li>
+            </ul>
+        </div>
+    </div>
+</footer>
--- resources/views/pages/homePage.blade.php
+++ resources/views/pages/homePage.blade.php
@@ -1,23 +1,53 @@
 @extends('layouts.main')
 @section('content')
     <main>
-        <div class="currentS">
-            <p>CURRENT SERIES</p>
-        </div>
-        <div class="series">
-            <div class="containerSeries">
-                @foreach ($data as $item)
-                    <div class="card">
-                        <div class="thumb">
-                            <img src="{{$item['thumb']}}" alt="">
-                        </div>
-                        <h3>{{$item['title']}}</h3>
-                    </div>
-                @endforeach
+        <section class="cardComics">
+            <div class="currentS">
+                <p>CURRENT SERIES</p>
             </div>
-        </div>
-        <div class="more">
-            <button>LOAD MORE</button>
-        </div>
+            <div class="series">
+                <div class="containerSeries">
+                    @foreach ($data as $item)
+                        <div class="card">
+                            <div class="thumb">
+                                <img src="{{$item['thumb']}}" alt="">
+                            </div>
+                            <h3>{{$item['title']}}</h3>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="more">
+                <button>LOAD MORE</button>
+            </div>
+        </section>
+        <section class="info">
+          
+            <div>
+                <img src="/storage/assets/buy-comics-digital-comics.png" alt="">
+                <p>DIGITAL COMICS</p>
+            </div>
+            
+            <div>
+                <img src="/storage/assets/buy-comics-merchandise.png" alt="">
+                <p>DC MERCHANDISE</p>
+            </div>
+            
+            <div>
+                <img src="/storage/assets/buy-comics-shop-locator.png" alt="">
+                <p>SUBSCRIPTION</p>
+            </div>
+            
+            <div>
+                <img src="/storage/assets/buy-comics-subscriptions.png" alt="">
+                <p>COMIC SHOP LOCATOR</p>
+            </div>
+            
+            <div>
+                <img src="/storage/assets/buy-dc-power-visa.svg" alt="">
+                <p>DC POWER VISA</p>    
+            </div>
+
+        </section>
     </main>
 @endsection
