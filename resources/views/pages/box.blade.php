629d8e2c2a84404c86ed0469638397ad4165a4d6
pasqu <pasquale.bucciero97@gmailcom>
2021-05-18 18:05:53 +0200

    main box completa

--- resources/views/pages/box.blade.php
+++ resources/views/pages/box.blade.php
@@ -34,47 +34,74 @@
                     <h2>Talent</h2>
                     <ul>
                        <li>
-                            <span>Art by:</span>
-                            @foreach ($card['artists'] as $artist)
-                                <span> {{$artist}} </span>
-                                @if ($loop->last)
-                                    .
-                                @else 
-                                    ,
-                                @endif
-                            @endforeach
+                            <span class="by">Art by:</span>
+                            <p>
+                                @foreach ($card['artists'] as $artist)
+                                    <span> {{$artist}} </span>
+                                    @if ($loop->last)
+                                        .
+                                    @else 
+                                        ,
+                                    @endif
+                                @endforeach
+                            </p>
                        </li>
                        <li>
-                           <span>Written by</span>
-                           @foreach ($card['writers'] as $writer)
-                               <span> {{$writer}}</span>
-                                @if ($loop->last)
-                                    .
-                                @else 
-                                    ,
-                                @endif
-                           @endforeach
+                           <span class="by">Written by:</span>
+                           <p>
+                               @foreach ($card['writers'] as $writer)
+                                   <span class="writer"> {{$writer}} </span>
+                                    @if ($loop->last)
+                                        .
+                                    @else 
+                                        ,
+                                    @endif
+                               @endforeach
+                           </p>
                        </li>
                     </ul>
                 </div>
                 <div class="specs">
-                    <h2>Specs</h2>
+                    <h2 >Specs</h2>
                     <ul>
                         <li>
-                            <span>Series:</span>
-                            <span> {{$card['series']}} </span>
+                            <span class="by">Series:</span>
+                            <span class="specsSeries"> {{$card['series']}} </span>
                         </li>
                         <li>
-                            <span>U.S. Price:</span>
-                            <span> {{$card['price']}} </span>
+                            <span class="by">U.S. Price:</span>
+                            <span class="specsPrice"> {{$card['price']}} </span>
                         </li>
                         <li>
-                            <span>On Sale Date:</span>
-                            <span> {{$card['sale_date']}} </span>
+                            <span class="by">On Sale Date:</span>
+                            <span class="specsSale"> {{$card['sale_date']}} </span>
                         </li>
                     </ul>
                 </div>
             </div>
+        </section>
+        <section class="info">
+          
+            <div>
+                <p>DIGITAL COMICS</p>
+                <img src="/storage/assets/buy-comics-digital-comics.png" alt="">
+            </div>
+            
+            <div>
+                <p>SHOP DC</p>
+                <img src="/storage/assets/buy-comics-merchandise.png" alt="">
+            </div>
+            
+            <div>
+                <p>COMIC SHOP LOCATOR</p>
+                <img src="/storage/assets/buy-comics-subscriptions.png" alt="">
+            </div>
+
+            <div>
+                <p>SUBSCRIPTION</p>
+                <img src="/storage/assets/buy-comics-shop-locator.png" alt="">
+            </div>
+
         </section>
     </main>
 @endsection
