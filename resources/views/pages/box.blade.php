9826852c1b7df69e3eabb50e002f913839804a51
pasqu <pasquale.bucciero97@gmailcom>
2021-05-18 16:27:38 +0200

    aggiunta section details

--- resources/views/pages/box.blade.php
+++ resources/views/pages/box.blade.php
@@ -28,5 +28,53 @@
                 </div>
             </div>
         </section>
+        <section class="details">
+            <div class="containerDetails">
+                <div class="talent">
+                    <h2>Talent</h2>
+                    <ul>
+                       <li>
+                            <span>Art by:</span>
+                            @foreach ($card['artists'] as $artist)
+                                <span> {{$artist}} </span>
+                                @if ($loop->last)
+                                    .
+                                @else 
+                                    ,
+                                @endif
+                            @endforeach
+                       </li>
+                       <li>
+                           <span>Written by</span>
+                           @foreach ($card['writers'] as $writer)
+                               <span> {{$writer}}</span>
+                                @if ($loop->last)
+                                    .
+                                @else 
+                                    ,
+                                @endif
+                           @endforeach
+                       </li>
+                    </ul>
+                </div>
+                <div class="specs">
+                    <h2>Specs</h2>
+                    <ul>
+                        <li>
+                            <span>Series:</span>
+                            <span> {{$card['series']}} </span>
+                        </li>
+                        <li>
+                            <span>U.S. Price:</span>
+                            <span> {{$card['price']}} </span>
+                        </li>
+                        <li>
+                            <span>On Sale Date:</span>
+                            <span> {{$card['sale_date']}} </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
